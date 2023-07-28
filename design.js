6a4b96a73637c0f9b7daec2d02e8b9872db44197
veronika sukantha <85723282+veronika-sukantha@users.noreply.github.com>
2023-07-28 18:57:05 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,54 @@
+ReactDOM.render(
+    <div>
+     <center>   
+     <u><h1>✒️BIOGRAPHY OF RABINDRANATH TAGORE✒️</h1></u>
+     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Rabindranath_Tagore.jpg/330px-Rabindranath_Tagore.jpg"  width="300" height="350" ></img>
+     </center>
+     <p><b>Rabindranath Tagore</b> was a famous Indian Bengali writer; he was a prolific writer and influential artist and a musician at the same time. He was born on May, 7 1861 and died on August 7, 1941. Tagore was a multi-talented personality with a strong desire to learn new things. He was also an educationist who established Viswa Bharati University. His contributions to the literature, music and other works are memorable.<br/>
+     <br/>
+     <b>Born:</b> 7 May 1861, Jorasanko Thakurbari, Kolkata<br/>
+<b>Died:</b> 7 August 1941, Jorasanko Thakurbari, Kolkata<br/>
+<b>Children:</b> Rathindranath Tagore, Shamindranath Tagore, Renuka Devi, Meera Devi, Madhurilata Devi<br/>
+<b>Spouse:</b> Mrinalini Devi (m. 1883–1902)<br/>
+<b>Parents: </b>Debendranath Tagore, Sarada Devi<br/></p>
+     <hr></hr>
+     <u><h3>BIRTH</h3></u>
+      <p>Rabindranath Tagore was born in 1861 </p>
+      <hr></hr>
+      <u><h3>CHILDHOOD</h3></u>
+      <p> He was educated at home; and although at seventeen he was sent to England for formal schooling, he did not finish his studies there</p>
+      <hr></hr>
+       <u><h3>TEENAGE</h3> </u>
+     <p> In his mature years, in addition to his many-sided literary activities, he managed the family estates, a project which brought him into close touch with common humanity and increased his interest in social reforms. He also started an experimental school at Shantiniketan where he tried his Upanishadic ideals of education. </p>
+     <hr></hr>
+     <u><h3>YOUTH</h3></u>
+     <center>
+     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Tagore_in_London_1879.jpg/330px-Tagore_in_London_1879.jpg" width="300" height="350"></img>
+     </center>
+     <p>In 1901 Tagore moved to Santiniketan to found an ashram with a marble-floored prayer hall—The Mandir—an experimental school, groves of trees, gardens, a library.[56] There his wife and two of his children died. His father died in 1905. He received monthly payments as part of his inheritance and income from the Maharaja of Tripura, sales of his family's jewellery, his seaside bungalow in Puri, and a derisory 2,000 rupees in book royalties.[57] He gained Bengali and foreign readers alike; he published Naivedya (1901) and Kheya (1906) and translated poems into free verse.
+       In 1912, Tagore translated his 1910 work Gitanjali into English. While on a trip to London, he shared these poems with admirers including William Butler Yeats and Ezra Pound. London's India Society published the work in a limited edition, and the American magazine Poetry published a selection from Gitanjali.[58] In November 1913, Tagore learned he had won that year's Nobel Prize in Literature: the Swedish Academy appreciated the idealistic—and for Westerners—accessible nature of a small body of his translated material focused on the 1912 Gitanjali: Song Offerings. He was awarded a knighthood by King George V in the 1915 Birthday Honours, but Tagore renounced it after the 1919 Jallianwala Bagh massacre.[60] Renouncing the knighthood, Tagore wrote in a letter addressed to Lord Chelmsford, the then British Viceroy of India, "The disproportionate severity of the punishments inflicted upon the unfortunate people and the methods of carrying them out, we are convinced, are without parallel in the history of civilised governments...The time has come when badges of honour make our shame glaring in their incongruous context of humiliation, and I for my part wish to stand, 
+       shorn of all special distinctions, by the side of my country men.<br/>
+</p>
+       <hr></hr>
+     <u><h3>WORKS</h3></u>
+     <center>
+     <img src="https://www.bl.uk/learning/timeline/external/1913-Tagore-Nobel-Prize-tl.jpg" width="350" height="350" ></img>
+     <p>Tagore had early success as a writer in his native Bengal. With his translations of some of his poems he became rapidly known in the West. In fact his fame attained a luminous height, taking him across continents on lecture tours and 
+      tours of friendship. For the world he became the voice of India’s spiritual heritage; and for India, especially for Bengal, he became a great living institution.</p>
+     </center>
+     <hr></hr>
+      <u><h3>AWARDS</h3></u>
+     <center> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Last_pic_of_Tagore.jpg/330px-Last_pic_of_Tagore.jpg"></img>
+
+     </center> 
+
+    <p><b>Nobel Prize in literature 1931</b><br/>
+    <b>knighthood<br/></b>
+    <b>Doctorate Of Literature</b>
+    </p>
+    <hr></hr>
+    <u><h3>DEATH</h3></u>
+      <p>Rabindranath Tagore died on August 7, 1941, at the age of 80. According to numerous credible reports, he died of prostate cancer. His death brought an end to a long period of agony. He died in the Jorasanko mansion, where he had grown up. </p>
+    </div>
+     ,
+     document.getElementById("content"));
